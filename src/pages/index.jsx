95c9c612f6bb22ae7f698f9af5bad6351710a14c
Felipe <felipealencar.0014@gmail.com>
2024-01-30 19:02:53 -0300

    Criando sessão welcome

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import styles from "./Home.module.scss";
 import Header from "./components/header";
+import Welcome from "./components/welcome";
 
 export default function Home() {
   return (
     <main className={`${styles.main}`}>
         <Header />
+        <Welcome />
     </main>
   );
 }
