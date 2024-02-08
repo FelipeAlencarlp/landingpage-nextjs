c9719f74b84ca83d72fa69a8997061769d9d427b
Felipe <felipealencar.0014@gmail.com>
2024-02-08 13:35:45 -0300

    Responsividade para celular concluido

--- src/pages/components/welcome/index.jsx
+++ src/pages/components/welcome/index.jsx
@@ -1,7 +1,6 @@
 import Button from '../button';
 import Styles from './welcome.module.scss';
 import BannerWelcome from '../../../../public/images/bannerWelcome.svg';
-import FundoWelcome from '../../../../public/images/fundoWelcome.svg';
 import Image from 'next/image';
 
 const Welcome = () => {
@@ -9,7 +8,9 @@ const Welcome = () => {
         <div className={Styles.container}>
             <div className={Styles.text}>
                 <h1>Melhor agência de marketing do bairro</h1>
-                <p>Somos uma agência de performance digital, aceleramos vendas e aquisição de leads para grandes marcas.</p>
+                <p>
+                    Somos uma agência de performance digital, aceleramos vendas e aquisição de leads para grandes marcas.
+                </p>
                 <Button title="Aumentar vendas" kind="secundary" />
             </div>
             <div className={Styles.image}>
