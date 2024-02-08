577d5dade9a13ac9356fed56cc4bd9c61e9816b0
Felipe <felipealencar.0014@gmail.com>
2024-02-08 15:00:18 -0300

    fontes adicionada

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,13 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head />
+            <link
+                rel="preconnect"
+                href="https://fonts.googleapis.com"
+            />
+            <link
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossorigin="true"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
+                rel="stylesheet"
+            />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
